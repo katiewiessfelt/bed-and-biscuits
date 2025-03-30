270dd1d67addbd0746119d86fb6706ff798812ec
Katie Wiessfelt <>
2025-03-30 15:18:52 -0500

    add routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,17 +2,32 @@ import Header from './Header/Header'
 import Footer from './Footer/Footer'
 import Home from './Home/Home'
 import Hero from './Hero/Hero'
+import Bakery from "./Bakery/Bakery";
+// import Boarding from "./Boarding/Boarding";
+// import About from "./About/About";
+// import Members from "./Members/Members";
 import './App.css'
+import { Routes, Route, useLocation } from "react-router-dom";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
+  const location = useLocation();
+  const showHero = location.pathname === "/";
   return (
-    <>
-      <Hero/>
+    <div>
+      {showHero && <Hero/>}
       <Header/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bakery" element={<Bakery />} />
+        {/* <Route path="/menu" element={<Menu />} /> */}
+        {/* <Route path="/boarding" element={<Boarding />} /> */}
+        {/* <Route path="/about" element={<About />} /> */}
+        {/* <Route path="/members" element={<Members />} /> */}
+      </Routes>
       <Footer/>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
